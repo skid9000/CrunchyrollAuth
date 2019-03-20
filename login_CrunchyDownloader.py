827cfa72df258aec29dd8b2bb53f749ce5f45c85
Skid <skid@tuto-craft.com>
2019-03-20 15:17:42 +0100

    Update the script + add requirements.

--- login_CrunchyDownloader.py
+++ login_CrunchyDownloader.py
@@ -2,25 +2,31 @@ import cfscrape
 import http.cookiejar
 from bs4 import BeautifulSoup
 import sys
+import requests
 
-scraper = cfscrape.create_scraper()
+headers = {
+	'Host': 'www.crunchyroll.com',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Language': '*',
+	'Referer': 'https://www.crunchyroll.com/login',
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Connection': 'keep-alive'
+}
 
-try:
-	scraper.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"
-except:
-	pass
+sess = requests.session()
+sess = cfscrape.create_scraper(sess)
 
 username = sys.argv[1]
 password = sys.argv[2]
 cpath = 'C:\\ProgramData\\Crunchy-DL\\cookies.txt'
 
-response = scraper.get("https://www.crunchyroll.com/login")
-
+response = sess.get("https://www.crunchyroll.com/login", headers=headers)
 cj = http.cookiejar.MozillaCookieJar(cpath)
-for c in scraper.cookies:
+for c in sess.cookies:
 	cj.set_cookie(c)
 
-soup = BeautifulSoup(response.text)
+soup = BeautifulSoup(response.text,features="html.parser")
 token = soup.find('input', {'name':'login_form[_token]'})['value']
 
 payload = {
@@ -30,10 +36,8 @@ payload = {
 	"login_form[_token]": token
 }
 
-scraper.post("https://www.crunchyroll.com/login", cookies=cj, data=payload)
-for c in scraper.cookies:
+sess.post(url="https://www.crunchyroll.com/login", cookies=cj, data=payload, headers=headers)
+for c in sess.cookies:
 	cj.set_cookie(c)
 
-print(cj)
-print(scraper.headers["User-Agent"])
 cj.save(cpath, True, True)
